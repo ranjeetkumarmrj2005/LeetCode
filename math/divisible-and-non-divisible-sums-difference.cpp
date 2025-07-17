72e05d8b35f55a67de3f5c3651bf78762787228b
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-17 14:18:21 +0530

    Synchronize existing submission history (3/3)

---
+++ math/divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+         int sumofdivisible=0;
+        int sumofnotdivisible=0;    
+        for(int i=1;i<=n;i++){
+            if(i%m!=0) sumofdivisible=i+sumofdivisible;
+            else  sumofnotdivisible=i+sumofnotdivisible;
+        }
+    return sumofdivisible-sumofnotdivisible;
+    }
+};
---
+++ math/reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int m=0;
+        while(x!=0){
+         if(m>INT_MAX/10||(m==INT_MAX/10&&x%10>7)||m<INT_MIN/10||(m==INT_MIN/10&&x%10<-8)) return 0;
+        m=m*10;
+        m=m+x%10;
+        x=x/10;
+        }
+        return m;
+        }
+};
