3a2b041883b86c92cda3a76e0e3f2603a709f136
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-18 08:30:55 +0530

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 13.4 MB]

--- linked-list/reverse-linked-list.cpp
+++ linked-list/reverse-linked-list.cpp
@@ -1,32 +1,16 @@
 
-
 class Solution {
 public:
-    ListNode* getNodeAtidx(ListNode* head,int idx){
-        ListNode* temp=head;
-        for(int i=1;i<idx;i++){
-            temp=temp->next;
-        }
-        return temp;
-    };
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp=head;
-        int n=0;
-        while(temp){
-            temp=temp->next;
-            n++;
+        ListNode* CURR=head;
+        ListNode* PREV=NULL;
+        ListNode* NEXT=head;
+        while(CURR){
+            NEXT=NEXT->next;
+            CURR->next=PREV;
+            PREV=CURR;
+            CURR=NEXT;
         }
-        int i=1;
-        int j=n;
-        while(i<j){
-            ListNode* left=getNodeAtidx(head,i);
-            ListNode* right=getNodeAtidx(head,j);
-            int t=left->val;
-            left->val=right->val;
-            right->val=t;
-            i++;
-            j--;
-        }
-        return head;
+        return PREV;
     }
 };
