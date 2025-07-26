6d7731a9686d228865322bd5b3317dd63055d0a7
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-26 11:28:04 +0530

    LC-remove-duplicates-from-sorted-list-ii [Runtime: 0 ms; Memory: 15.8 MB]

---
+++ linked-list/remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* PREV=dummy;
+        ListNode* CURR=head;
+        while(CURR!=NULL){
+            if(CURR->next!=NULL && CURR->val==CURR->next->val){
+                while(CURR->next!=NULL && CURR->val==CURR->next->val){
+                    CURR=CURR->next;
+                }
+                PREV->next=CURR->next;
+            }
+            else PREV=CURR;
+            CURR=CURR->next;
+        }
+        return dummy->next;
+    }
+};
