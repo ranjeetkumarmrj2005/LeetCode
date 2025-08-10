9a3164d159f8a8fe2a202442ec6dddecd63d022e
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-08-10 12:05:00 +0530

    LC-single-number [Runtime: 0 ms; Memory: 20.6 MB]

--- bit-manipulation/single-number.cpp
+++ bit-manipulation/single-number.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        sort(nums.begin(), nums.end());
+        int x=0;
         int i=0;
-        int j=1;
-        while(j<nums.size()){
-            if(nums[i]!=nums[j]) return nums[i];
-            i=i+2;
-            j=j+2;
+        while(i<nums.size()){
+            x=x^nums[i];
+            i++;
         }
-        return nums[i];
-        
+        return x;
     }
 };
