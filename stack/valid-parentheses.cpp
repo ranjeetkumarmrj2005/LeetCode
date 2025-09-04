dd556f476455c604bc134e12e6ec20d66b15b525
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-09-04 17:36:20 +0530

    LC-valid-parentheses [Runtime: 0 ms; Memory: 8.9 MB]

--- stack/valid-parentheses.cpp
+++ stack/valid-parentheses.cpp
@@ -9,6 +9,7 @@ public:
                 st.push(s[i]);
             }
             else {
+                if(st.size()==0) return false;
                 if( (s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{') ) st.pop();
                 else return false;
             }
