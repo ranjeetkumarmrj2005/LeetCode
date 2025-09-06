023485fae38867a0d4e5ddae9debdbf276710166
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-09-06 15:57:46 +0530

    LC-number-of-visible-people-in-a-queue [Runtime: 11 ms; Memory: 88.7 MB]

--- monotonic-stack/number-of-visible-people-in-a-queue.cpp
+++ monotonic-stack/number-of-visible-people-in-a-queue.cpp
@@ -7,23 +7,13 @@ public:
         st.push(heights[n-1]);
         for(int i=n-2;i>=0;i--){
             int see=0;
-            stack<int>temp;
             while(st.size()>0 && heights[i]>=st.top()){
-                int x=st.top();
-                temp.push(x);
-                st.pop();
-                if( st.size()>0 && x<=st.top()){
-                    see++;
-                }
-                
+                st.pop();  
+                see++;  
             }
-            if(st.size()==0) ans[i]=see+1;
+            if(st.size()==0) ans[i]=see;
             else ans[i]=see+1;
-
-            while(temp.size()>0){
-                st.push(temp.top());
-                temp.pop();
-            }
+            
             st.push(heights[i]);
 
         }
