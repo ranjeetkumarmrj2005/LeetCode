0ac1d505cc7fa91020734667896a043c51c81cb3
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2026-01-19 22:55:01 +0530

    LC-sliding-window-maximum [Runtime: 1389 ms; Memory: 138.6 MB]

--- queue/sliding-window-maximum.cpp
+++ queue/sliding-window-maximum.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if(k==1) return nums;
-        vector<int>ans;
         int n=nums.size();
-        int i=0;
-        int j=k;
-        while(j<=n){
-        int maximum=INT_MIN;
-            for(int l=i;l<j;l++){
-                maximum=max(maximum,nums[l]);
-            }
-            ans.push_back(maximum);
-            i++;
-            j++;
+        vector<int>ans;
+        int NGIdx[n];
+        stack<int>st;
+        st.push(n-1);
+        NGIdx[n-1]=n;
+        for(int i=n-2;i>=0;i--){
+        while(st.size()>0 && nums[st.top()]<=nums[i]){
+            st.pop();
         }
-        return ans;
+        if(st.size()==0) NGIdx[i]=n;
+        else NGIdx[i]=st.top();
+        st.push(i);
+        }
+        for(int i=0;i<=n-k;i++){
+            int mx=nums[i];
+            int j=i;
+            while(j<i+k){
+                mx=nums[j];
+                j=NGIdx[j];
+            }
+            ans.push_back(mx);
+        }
+       return ans;
     }
 };
