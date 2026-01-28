bd73083fad7a4ace66051a67ba5aef4be50a0216
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2026-01-28 19:29:35 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: 5 ms; Memory: 97.4 MB]

--- array/best-time-to-buy-and-sell-stock.cpp
+++ array/best-time-to-buy-and-sell-stock.cpp
@@ -3,21 +3,13 @@ public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         int minPrice=INT_MAX;
-        int minidx;
+        int maxprofit=0;
         for(int i=0;i<n;i++){
             if(minPrice>prices[i]) {
                 minPrice=prices[i];
-                minidx=i;
             }
+            else maxprofit=max(maxprofit,prices[i]-minPrice);
         }
-        if(minidx==n-1) return 0;
-        int maxPrice=INT_MIN;
-        for(int i=minidx+1;i<n;i++){
-            if(maxPrice<prices[i]) {
-                maxPrice=prices[i];
-            }
-        }
-        if(maxPrice-minPrice>0) return maxPrice-minPrice;
-        else return 0;
+        return maxprofit;
     }
 };
